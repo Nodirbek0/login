82eff3d587ade201b5961b752584d811a402463b
nodirbek0 <you@gmail.com>
2021-10-10 23:58:44 +0500

    chuvaak

--- login.py
+++ login.py
@@ -1,4 +1,13 @@
 print("Hello ")
 
-
-
+   my_database = mysql.connector.connect(
+            host="localhost",
+            user="flash",
+            password="nod12345",
+            database="dang"
+        )
+        mysql_db = my_database.cursor()
+        query = f"insert into login(name, login, password, age) values" \
+                f"('{self.name}', '{self.login}', '{self.password}', {self.age})"
+        mysql_db.execute(query)
+        my_database.commit()
